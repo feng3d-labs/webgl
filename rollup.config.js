d3355331b8791402d601badfa8168a36ee383514
feng <908087098@qq.com>
2022-08-27 16:13:50 +0800

    const module = '.lib/index.es.js';

--- rollup.config.js
+++ rollup.config.js
@@ -70,7 +70,6 @@ async function main()
     const input = path.join(basePath, 'src/index.ts');
 
     const {
-        module,
         bundle,
         bundleInput,
         bundleOutput,
@@ -79,6 +78,7 @@ async function main()
     } = pkg;
     const types = pkg['bundle:types'];
     const main = './lib/index.js';
+    const module = '.lib/index.es.js';
     const freeze = false;
 
     results.push({
