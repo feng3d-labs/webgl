25334b6aa63defb7d07fb7214ee70024ecfec980
feng <908087098@qq.com>
2024-01-17 20:43:58 +0800

    Texture.setTextureData

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from './data/Texture';
 export * from './RenderBuffer';
 export * from './WebGLRenderer';
 export * from './data/AttributeBuffer';
@@ -6,6 +5,7 @@ export * from './data/ElementBuffer';
 export * from './data/RenderAtomic';
 export * from './data/RenderParams';
 export * from './data/Shader';
+export * from './data/Texture';
 export * from './data/Uniforms';
 export * from './gl/WebGLCapabilities';
 export * from './gl/WebGLEnums';
@@ -14,4 +14,6 @@ export * from './gl/WebGLUniforms';
 export * from './shader/Macro';
 export * from './shader/ShaderLib';
 export * from './shader/ShaderMacroUtils';
+export * from './textures/RenderTargetTexture2D';
+export * from './textures/Texture2D';
 
