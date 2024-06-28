9b8ed81f079b1e542435caec76423eb39f48dad1
feng <908087098@qq.com>
2024-06-28 16:32:18 +0800

    runRenderPipeline

--- src/data/IDepthStencilState.ts
+++ src/data/IDepthStencilState.ts
@@ -1,4 +1,20 @@
-import { DepthFunc } from "./RenderParams";
+/**
+ * 指定深度比较函数的枚举，该函数设置绘制像素的条件，默认 LESS，如果传入值小于深度缓冲区值则通过。
+ *
+ * A GLenum specifying the depth comparison function, which sets the conditions under which the pixel will be drawn. The default value is gl.LESS.
+ *
+ * * `NEVER` 总是不通过。
+ * * `LESS` 如果传入值小于深度缓冲区值则通过。
+ * * `EQUAL` 如果传入值等于深度缓冲区值则通过。
+ * * `LEQUAL` 如果传入值小于或等于深度缓冲区值则通过。
+ * * `GREATER` 如果传入值大于深度缓冲区值则通过。
+ * * `NOTEQUAL` 如果传入值不等于深度缓冲区值则通过。
+ * * `GEQUAL` 如果传入值大于或等于深度缓冲区值则通过。
+ * * `ALWAYS` 总是通过。
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/depthFunc
+ */
+export type DepthFunc = "NEVER" | "LESS" | "EQUAL" | "LEQUAL" | "GREATER" | "NOTEQUAL" | "GEQUAL" | "ALWAYS";
 
 export interface IDepthStencilState
 {
--- src/data/RenderParams.ts
+++ src/data/RenderParams.ts
@@ -310,24 +310,6 @@ export type BlendEquation = "FUNC_ADD" | "FUNC_SUBTRACT" | "FUNC_REVERSE_SUBTRAC
 export type BlendFactor = "ZERO" | "ONE" | "SRC_COLOR" | "ONE_MINUS_SRC_COLOR" | "DST_COLOR" | "ONE_MINUS_DST_COLOR" | "SRC_ALPHA" | "ONE_MINUS_SRC_ALPHA" | "DST_ALPHA" | "ONE_MINUS_DST_ALPHA" | "SRC_ALPHA_SATURATE";
 
 /**
- * 指定深度比较函数的枚举，该函数设置绘制像素的条件，默认 LESS，如果传入值小于深度缓冲区值则通过。
- *
- * A GLenum specifying the depth comparison function, which sets the conditions under which the pixel will be drawn. The default value is gl.LESS.
- *
- * * `NEVER` 总是不通过。
- * * `LESS` 如果传入值小于深度缓冲区值则通过。
- * * `EQUAL` 如果传入值等于深度缓冲区值则通过。
- * * `LEQUAL` 如果传入值小于或等于深度缓冲区值则通过。
- * * `GREATER` 如果传入值大于深度缓冲区值则通过。
- * * `NOTEQUAL` 如果传入值不等于深度缓冲区值则通过。
- * * `GEQUAL` 如果传入值大于或等于深度缓冲区值则通过。
- * * `ALWAYS` 总是通过。
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/depthFunc
- */
-export type DepthFunc = "NEVER" | "LESS" | "EQUAL" | "LEQUAL" | "GREATER" | "NOTEQUAL" | "GEQUAL" | "ALWAYS";
-
-/**
  * A GLenum specifying the test function. The default function is gl.ALWAYS.
  *
  * * `NEVER` 总是不通过。
