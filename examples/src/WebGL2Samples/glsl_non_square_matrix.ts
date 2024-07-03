614de990264128587a8f04ed376e2d7f6cd1034b
feng <908087098@qq.com>
2024-07-03 10:12:31 +0800

    glsl_non_square_matrix

---
+++ examples/src/WebGL2Samples/glsl_non_square_matrix.ts
@@ -0,0 +1,101 @@
+const canvas = document.createElement("canvas");
+canvas.width = Math.min(window.innerWidth, window.innerHeight);
+canvas.height = canvas.width;
+document.body.appendChild(canvas);
+
+const gl = canvas.getContext("webgl2", { antialias: false });
+const isWebGL2 = !!gl;
+if (!isWebGL2)
+{
+    document.getElementById("info").innerHTML = "WebGL 2 is not available.  See <a href=\"https://www.khronos.org/webgl/wiki/Getting_a_WebGL_Implementation\">How to get a WebGL 2 implementation</a>";
+
+    return;
+}
+
+// -- Init program
+const program = createProgram(gl, getShaderSource("vs"), getShaderSource("fs"));
+const mvpLocation = gl.getUniformLocation(program, "MVP");
+const diffuseLocation = gl.getUniformLocation(program, "diffuse");
+
+// -- Init buffers: vec2 Position, vec2 Texcoord
+const positions = new Float32Array([
+    -1.0, -1.0,
+    1.0, -1.0,
+    1.0, 1.0,
+    1.0, 1.0,
+    -1.0, 1.0,
+    -1.0, -1.0
+]);
+const vertexPosBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+const texCoords = new Float32Array([
+    0.0, 1.0,
+    1.0, 1.0,
+    1.0, 0.0,
+    1.0, 0.0,
+    0.0, 0.0,
+    0.0, 1.0
+]);
+const vertexTexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+// -- Init VertexArray
+const vertexArray = gl.createVertexArray();
+gl.bindVertexArray(vertexArray);
+
+const vertexPosLocation = 0; // set with GLSL layout qualifier
+gl.enableVertexAttribArray(vertexPosLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
+gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+const vertexTexLocation = 1; // set with GLSL layout qualifier
+gl.enableVertexAttribArray(vertexTexLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
+gl.vertexAttribPointer(vertexTexLocation, 2, gl.FLOAT, false, 0, 0);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+gl.bindVertexArray(null);
+
+loadImage("../assets/img/Di-3d.png", function (image)
+{
+    // -- Init Texture
+    const texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+    // -- Render
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const matrix = new Float32Array([
+        0.5, 0.0, 0.0,
+        0.0, 0.5, 0.0,
+        0.0, 0.0, 0.5,
+        0.2, -0.2, 0.0 //translation
+    ]);
+
+    gl.useProgram(program);
+    gl.uniformMatrix4x3fv(mvpLocation, false, matrix);
+    gl.uniform1i(diffuseLocation, 0);
+
+    gl.bindVertexArray(vertexArray);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    // Delete WebGL resources
+    gl.deleteBuffer(vertexPosBuffer);
+    gl.deleteBuffer(vertexTexBuffer);
+    gl.deleteTexture(texture);
+    gl.deleteProgram(program);
+    gl.deleteVertexArray(vertexArray);
+});
