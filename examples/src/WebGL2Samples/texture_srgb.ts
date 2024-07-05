78514ea182bb758ffd614179214ef79d4afcc1a5
feng <908087098@qq.com>
2024-07-05 11:29:52 +0800

    texture_srgb

--- examples/src/WebGL2Samples/texture_srgb.ts
+++ examples/src/WebGL2Samples/texture_srgb.ts
@@ -1,25 +1,21 @@
+import { IProgram, IRenderPass, IRenderingContext, ISampler, ITexture, IVertexArrayObject, IVertexBuffer, WebGL } from "../../../src";
+import { getShaderSource, loadImage } from "./utility";
+
 (function ()
 {
     const canvas = document.createElement("canvas");
+    canvas.id = "glcanvas";
     canvas.width = Math.min(window.innerWidth, window.innerHeight);
     canvas.height = canvas.width;
     document.body.appendChild(canvas);
 
-    const gl = canvas.getContext("webgl2", { antialias: false });
-    const isWebGL2 = !!gl;
-    if (!isWebGL2)
-    {
-        document.getElementById("info").innerHTML = "WebGL 2 is not available.  See <a href=\"https://www.khronos.org/webgl/wiki/Getting_a_WebGL_Implementation\">How to get a WebGL 2 implementation</a>";
-
-        return;
-    }
+    const rc: IRenderingContext = { canvasId: "glcanvas", contextId: "webgl2", antialias: false };
 
     // -- Initialize program
 
-    const program = createProgram(gl, getShaderSource("vs"), getShaderSource("fs"));
-
-    const uniformMvpLocation = gl.getUniformLocation(program, "mvp");
-    const uniformDiffuseLocation = gl.getUniformLocation(program, "material.diffuse");
+    const program: IProgram = {
+        vertex: { code: getShaderSource("vs") }, fragment: { code: getShaderSource("fs") },
+    };
 
     // -- Initialize buffer
 
@@ -31,10 +27,7 @@
         -1.0, 1.0,
         -1.0, -1.0
     ]);
-    const vertexPosBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    const vertexPosBuffer: IVertexBuffer = { target: "ARRAY_BUFFER", data: positions, usage: "STATIC_DRAW" };
 
     const texcoords = new Float32Array([
         0.0, 1.0,
@@ -44,48 +37,32 @@
         0.0, 0.0,
         0.0, 1.0
     ]);
-    const vertexTexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, texcoords, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    const vertexTexBuffer: IVertexBuffer = { target: "ARRAY_BUFFER", data: texcoords, usage: "STATIC_DRAW" };
 
     // -- Initialize vertex array
 
-    const vertexArray = gl.createVertexArray();
-    gl.bindVertexArray(vertexArray);
-
-    const vertexPosLocation = 0; // set with GLSL layout qualifier
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-    gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vertexPosLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    const vertexTexLocation = 4; // set with GLSL layout qualifier
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
-    gl.vertexAttribPointer(vertexTexLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vertexTexLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    gl.bindVertexArray(null);
+    const vertexArray: IVertexArrayObject = {
+        vertices: {
+            position: { buffer: vertexPosBuffer, numComponents: 2 },
+            textureCoordinates: { buffer: vertexTexBuffer, numComponents: 2 },
+        }
+    };
 
     // -- Load texture then render
 
-    const imageUrl = "../assets/img/Di-3d.png";
-    let texture;
+    const imageUrl = "../../assets/img/Di-3d.png";
+    let texture: ITexture;
+    let sampler: ISampler;
     loadImage(imageUrl, function (image)
     {
-        texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(
-            gl.TEXTURE_2D,
-            0, // Level of details
-            gl.SRGB8, // Format
-            gl.RGB,
-            gl.UNSIGNED_BYTE, // Size of each channel
-            image
-        );
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        texture = {
+            target: "TEXTURE_2D",
+            internalformat: "SRGB8",
+            format: "RGB",
+            type: "UNSIGNED_BYTE",
+            sources: [{ level: 0, source: image }],
+        };
+        sampler = { minFilter: "NEAREST", magFilter: "NEAREST" };
 
         render();
     });
@@ -93,11 +70,10 @@
     function render()
     {
         // Clear color buffer
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        // Bind program
-        gl.useProgram(program);
+        const rp: IRenderPass = {
+            passDescriptor: { colorAttachments: [{ clearValue: [0.0, 0.0, 0.0, 1.0], loadOp: "clear" }] },
+            renderObjects: [],
+        };
 
         const matrix = new Float32Array([
             0.5, 0.0, 0.0, 0.0,
@@ -105,18 +81,22 @@
             0.0, 0.0, 0.5, 0.0,
             0.0, 0.0, 0.0, 1.0
         ]);
-        gl.uniformMatrix4fv(uniformMvpLocation, false, matrix);
-        gl.uniform1i(uniformDiffuseLocation, 0);
-
-        gl.bindVertexArray(vertexArray);
-
-        gl.drawArraysInstanced(gl.TRIANGLES, 0, 6, 1);
+        rp.renderObjects.push({
+            pipeline: program,
+            vertexArray,
+            uniforms: {
+                mvp: matrix,
+                material: { diffuse: { texture, sampler } },
+            },
+            drawArrays: { vertexCount: 6 },
+        });
+        WebGL.runRenderPass(rc, rp);
 
         // Cleanup
-        gl.deleteBuffer(vertexPosBuffer);
-        gl.deleteBuffer(vertexTexBuffer);
-        gl.deleteVertexArray(vertexArray);
-        gl.deleteTexture(texture);
-        gl.deleteProgram(program);
+        WebGL.deleteBuffer(rc, vertexPosBuffer);
+        WebGL.deleteBuffer(rc, vertexTexBuffer);
+        WebGL.deleteVertexArray(rc, vertexArray);
+        WebGL.deleteTexture(rc, texture);
+        WebGL.deleteProgram(rc, program);
     }
 })();
