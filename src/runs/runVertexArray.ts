ab120f10a39f2f6873e26ff1e297e2101e5258e4
feng <908087098@qq.com>
2024-07-06 00:40:42 +0800

    runVertexAttribute

--- src/runs/runVertexArray.ts
+++ src/runs/runVertexArray.ts
@@ -1,3 +1,4 @@
+import { watcher } from "@feng3d/watcher";
 import { getProgram } from "../caches/getProgram";
 import { IRenderPipeline } from "../data/IRenderPipeline";
 import { IVertexArrayObject } from "../data/IVertexArrayObject";
@@ -10,6 +11,14 @@ declare global
     {
         _vertexArrays: Map<IVertexArrayObject, WebGLVertexArrayObject>;
     }
+
+    interface WebGLVertexArrayObject
+    {
+        /**
+         * 销毁。
+         */
+        destroy: () => void;
+    }
 }
 
 /**
@@ -19,21 +28,26 @@ export function runVertexArray(gl: WebGLRenderingContext, pipeline: IRenderPipel
 {
     if (!vertexArray) return;
 
-    // let webGLVertexArrayObject: WebGLVertexArrayObject;
-    // if (gl instanceof WebGL2RenderingContext)
-    // {
-    //     webGLVertexArrayObject = gl._vertexArrays.get(vertexArray);
-    //     if (webGLVertexArrayObject)
-    //     {
-    //         gl.bindVertexArray(webGLVertexArrayObject);
+    let webGLVertexArrayObject: WebGLVertexArrayObject;
+    if (gl instanceof WebGL2RenderingContext)
+    {
+        webGLVertexArrayObject = gl._vertexArrays.get(vertexArray);
+        if (webGLVertexArrayObject)
+        {
+            gl.bindVertexArray(webGLVertexArrayObject);
 
-    //         return;
-    //     }
+            return;
+        }
 
-    //     webGLVertexArrayObject = gl.createVertexArray();
-    //     gl.bindVertexArray(webGLVertexArrayObject);
-    //     gl._vertexArrays.set(vertexArray, webGLVertexArrayObject);
-    // }
+        webGLVertexArrayObject = gl.createVertexArray();
+        gl.bindVertexArray(webGLVertexArrayObject);
+        gl._vertexArrays.set(vertexArray, webGLVertexArrayObject);
+    }
+
+    const onChanged = () =>
+    {
+        deleteVertexArray(gl, vertexArray);
+    };
 
     //
     const { vertices, index } = vertexArray;
@@ -53,8 +67,28 @@ export function runVertexArray(gl: WebGLRenderingContext, pipeline: IRenderPipel
             console.error(`缺少顶点 ${name} 数据！`);
         }
 
+        watcher.watch(attribute, "divisor", onChanged);
+
         runVertexAttribute(gl, location, attribute);
     });
+
+    webGLVertexArrayObject.destroy = () =>
+    {
+        shaderResult.attributes.forEach((activeInfo) =>
+        {
+            const { name, location } = activeInfo;
+            // 处理 WebGL 内置属性 gl_VertexID 等
+            if (location < 0) return;
+
+            const attribute = vertices[name];
+            if (!attribute)
+            {
+                console.error(`缺少顶点 ${name} 数据！`);
+            }
+
+            watcher.unwatch(attribute, "divisor", onChanged);
+        });
+    };
 
     runIndexBuffer(gl, index);
 }
@@ -66,5 +100,6 @@ export function deleteVertexArray(gl: WebGLRenderingContext, vertexArray: IVerte
         const webGLVertexArrayObject = gl._vertexArrays.get(vertexArray);
         gl._vertexArrays.delete(vertexArray);
         gl.deleteVertexArray(webGLVertexArrayObject);
+        webGLVertexArrayObject.destroy();
     }
 }
