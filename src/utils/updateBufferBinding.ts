7dea704a68618348d2c0d262d79830089ecd4938
feng <908087098@qq.com>
2025-01-08 17:53:05 +0800

    updateBufferBinding

--- src/utils/updateBufferBinding.ts
+++ src/utils/updateBufferBinding.ts
@@ -35,28 +35,48 @@ export function updateBufferBinding(uniformBlock: IUniformBlockInfo, uniformData
         uniformInfo.items.forEach((itemInfo) =>
         {
             currentSize = roundUp(alignSize.align, currentSize); // 单项对齐
-            itemInfo.offset = currentSize;
-            itemInfo.size = alignSize.size;
+            const itemInfoOffset = currentSize;
+            const itemInfoSize = alignSize.size;
             //
             currentSize += alignSize.size;
+            const Cls = alignSize.clsType;
             //
             const paths = itemInfo.paths;
             const update = () =>
             {
-                let value: any;
+                let value: any = uniformData;
                 for (let i = 0; i < paths.length; i++)
                 {
-                    value = uniformData[paths[i]];
+                    value = value[paths[i]];
                     if (value === undefined) return;
                 }
 
+                let data: Float32Array | Int32Array | Uint32Array;
+                if (typeof value === "number")
+                {
+                    data = new Cls([value]);
+                }
+                else if (value.constructor.name !== Cls.name)
+                {
+                    data = new Cls(value as ArrayLike<number>);
+                }
+                else
+                {
+                    data = value as any;
+                }
+
+                const writeBuffers = buffer.writeBuffers ?? [];
+                writeBuffers.push({ data: data.buffer, bufferOffset: offset + itemInfoOffset, size: Math.min(itemInfoSize, data.byteLength) });
+                buffer.writeBuffers = writeBuffers;
             }
 
             update();
             watcher.watchchain(uniformData, itemInfo.paths.join("."), update, undefined, false);
         });
     });
-    currentSize = roundUp(4, currentSize); // 整个数据对齐
+    currentSize = roundUp(16, currentSize); // 整个数据对齐
+
+    console.assert(size === currentSize, `uniformBlock映射尺寸出现错误( ${size}  ${currentSize} )！`);
 }
 
 function roundUp(k: number, n: number): number
@@ -82,30 +102,36 @@ export interface IBufferBinding
  * @see https://github.com/brendan-duncan/wgsl_reflect/blob/main/src/wgsl_reflect.ts#L1206
  * @see https://www.orillusion.com/zh/wgsl.html#memory-layouts
  */
-const uniformBufferTypeAlignSizeMap: { [key: string]: { align: number, size: number } } = {
-    "FLOAT": { align: 4, size: 4 },
-    "FLOAT_VEC2": { align: 8, size: 8 },
-    "FLOAT_VEC3": { align: 16, size: 12 },
-    "FLOAT_VEC4": { align: 16, size: 16 },
-    "INT": { align: 4, size: 4 },
-    "INT_VEC2": { align: 8, size: 8 },
-    "INT_VEC3": { align: 16, size: 12 },
-    "INT_VEC4": { align: 16, size: 16 },
-    "BOOL": { align: 4, size: 4 },
-    "BOOL_VEC2": { align: 8, size: 8 },
-    "BOOL_VEC3": { align: 16, size: 12 },
-    "BOOL_VEC4": { align: 16, size: 16 },
-    "FLOAT_MAT2": { align: 8, size: 16 },
-    "FLOAT_MAT3": { align: 16, size: 48 },
-    "FLOAT_MAT4": { align: 16, size: 64 },
-    "UNSIGNED_INT": { align: 4, size: 4 },
-    "UNSIGNED_INT_VEC2": { align: 8, size: 8 },
-    "UNSIGNED_INT_VEC3": { align: 16, size: 12 },
-    "UNSIGNED_INT_VEC4": { align: 16, size: 16 },
-    "FLOAT_MAT2x3": { align: 16, size: 32 },
-    "FLOAT_MAT2x4": { align: 16, size: 32 },
-    "FLOAT_MAT3x2": { align: 8, size: 24 },
-    "FLOAT_MAT3x4": { align: 16, size: 48 },
-    "FLOAT_MAT4x2": { align: 8, size: 32 },
-    "FLOAT_MAT4x3": { align: 16, size: 64 },
+const uniformBufferTypeAlignSizeMap: {
+    [key: string]: {
+        align: number,
+        size: number,
+        clsType: Float32ArrayConstructor | Int32ArrayConstructor | Uint32ArrayConstructor,
+    }
+} = {
+    "FLOAT": { align: 4, size: 4, clsType: Float32Array },
+    "FLOAT_VEC2": { align: 8, size: 8, clsType: Float32Array },
+    "FLOAT_VEC3": { align: 16, size: 12, clsType: Float32Array },
+    "FLOAT_VEC4": { align: 16, size: 16, clsType: Float32Array },
+    "INT": { align: 4, size: 4, clsType: Int32Array },
+    "INT_VEC2": { align: 8, size: 8, clsType: Int32Array },
+    "INT_VEC3": { align: 16, size: 12, clsType: Int32Array },
+    "INT_VEC4": { align: 16, size: 16, clsType: Int32Array },
+    "BOOL": { align: 4, size: 4, clsType: Int32Array },
+    "BOOL_VEC2": { align: 8, size: 8, clsType: Int32Array },
+    "BOOL_VEC3": { align: 16, size: 12, clsType: Int32Array },
+    "BOOL_VEC4": { align: 16, size: 16, clsType: Int32Array },
+    "FLOAT_MAT2": { align: 8, size: 16, clsType: Float32Array },
+    "FLOAT_MAT3": { align: 16, size: 48, clsType: Float32Array },
+    "FLOAT_MAT4": { align: 16, size: 64, clsType: Float32Array },
+    "UNSIGNED_INT": { align: 4, size: 4, clsType: Uint32Array },
+    "UNSIGNED_INT_VEC2": { align: 8, size: 8, clsType: Uint32Array },
+    "UNSIGNED_INT_VEC3": { align: 16, size: 12, clsType: Uint32Array },
+    "UNSIGNED_INT_VEC4": { align: 16, size: 16, clsType: Uint32Array },
+    "FLOAT_MAT2x3": { align: 16, size: 32, clsType: Float32Array },
+    "FLOAT_MAT2x4": { align: 16, size: 32, clsType: Float32Array },
+    "FLOAT_MAT3x2": { align: 8, size: 24, clsType: Float32Array },
+    "FLOAT_MAT3x4": { align: 16, size: 48, clsType: Float32Array },
+    "FLOAT_MAT4x2": { align: 8, size: 32, clsType: Float32Array },
+    "FLOAT_MAT4x3": { align: 16, size: 64, clsType: Float32Array },
 };
