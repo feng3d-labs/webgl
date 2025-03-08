18c71ca799df99eb3f7bdd957d2def3d5c44dfa3
feng <908087098@qq.com>
2025-03-09 01:34:57 +0800

    refactor(getGLCanvasContext): 重构获取 WebGL 上下文逻辑
    - 修改了 getWebGLContext 函数的实现，优化了获取 WebGL 上下文的流程
    - 重构了 GLCanvasContext 类，改为接口和类的组合结构
    - 引入了 DataProxy 模式，用于初始化和管理 GLCanvasContext 实例
    - 优化了警告和错误信息的输出格式

--- src/data/GLCanvasContext.ts
+++ src/data/GLCanvasContext.ts
@@ -1,11 +1,11 @@
-import { Data } from "@feng3d/render-api";
+import { DataProxy } from "@feng3d/render-api";
 
 /**
  * 画布(WebGL)上下文信息。
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
  */
-export class GLCanvasContext extends Data implements WebGLContextAttributes
+export interface GLCanvasContext extends WebGLContextAttributes
 {
     /**
      * 画布编号。
@@ -15,13 +15,37 @@ export class GLCanvasContext extends Data implements WebGLContextAttributes
     /**
      * WebGL上下文类型
      */
-    contextId?: "webgl" | "webgl2" = "webgl2";
+    contextId?: "webgl" | "webgl2";
 
-    depth?: boolean = true;
-    stencil?: boolean = true;
-    antialias?: boolean = false;
-    premultipliedAlpha?: boolean = true;
-    preserveDrawingBuffer?: boolean = false;
-    powerPreference?: WebGLPowerPreference = "default";
-    failIfMajorPerformanceCaveat?: boolean = false;
+    depth?: boolean;
+    stencil?: boolean;
+    antialias?: boolean;
+    premultipliedAlpha?: boolean;
+    preserveDrawingBuffer?: boolean;
+    powerPreference?: WebGLPowerPreference;
+    failIfMajorPerformanceCaveat?: boolean;
 }
+
+export class GLCanvasContext
+{
+    static addInitFunc: (func: (material: GLCanvasContext) => ((material: GLCanvasContext) => void)) => void = DataProxy.addInitFunc;
+    static init: (material: Partial<GLCanvasContext>) => GLCanvasContext = DataProxy.init;
+    static del: (material: GLCanvasContext) => GLCanvasContext = DataProxy.del;
+}
+
+GLCanvasContext.addInitFunc((context) =>
+{
+    context.contextId ??= "webgl2";
+    context.depth ??= true;
+    context.stencil ??= true;
+    context.antialias ??= false;
+    context.premultipliedAlpha ??= true;
+    context.preserveDrawingBuffer ??= false;
+    context.powerPreference ??= "default";
+    context.failIfMajorPerformanceCaveat ??= false;
+
+    return (material) =>
+    {
+
+    };
+});
