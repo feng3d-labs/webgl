45239dd6b4c08f095ec3dbef68fc2cbfea146f5d
feng <908087098@qq.com>
2026-02-07 11:07:31 +0800

    chore: 更新 .gitignore 和 Vite 配置，添加开发环境定义，更新子模块至最新提交

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
         outDir: 'public',
     },
     base: './',
+    define: {
+        __DEV__: 'true',
+    },
     plugins: [
         shaderToString(),
     ],
